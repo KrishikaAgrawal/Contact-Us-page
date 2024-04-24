15530d638225fc40b1bc8940a04cb2b86ca55b47
Krishi <krishi@gmail.com>
2024-04-24 19:47:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ContactUs from "./component/Contact/ContactUs";
+import Event from "./component/Events/Event";
+
+function App() {
+  return (
+    <>
+      {/* <ContactUs /> */}
+      <Event />
+    </>
+  );
+}
+
+export default App;
